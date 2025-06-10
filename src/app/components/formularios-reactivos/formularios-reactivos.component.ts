c703089fa1865f31326a2f1d6d22a5edbadd834d
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2025-06-09 19:09:07 -0500

    formularios reactivos with angular

---
+++ src/app/components/formularios-reactivos/formularios-reactivos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularios-reactivos',
+  imports: [],
+  templateUrl: './formularios-reactivos.component.html',
+  styleUrl: './formularios-reactivos.component.css'
+})
+export class FormulariosReactivosComponent {
+
+}
